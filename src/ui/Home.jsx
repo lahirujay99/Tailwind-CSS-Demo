06cb59a1c61da37bc743a4ae647d449dd068113a
lahirujay99 <lahirujayawardana94@gmail.com>
2023-07-10 21:42:07 +0530

    change text color in home.jsx

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,10 +1,10 @@
 function Home() {
   return (
     <div>
-      <h1 className="text-center text-xl font-semibold text-yellow-500">
+      <h1 className="text-center text-xl font-semibold text-stone-700">
         The best pizza.
         <br />
-        <span>Straight out of the oven, straight to you.</span>
+        <span className="text-yellow-500">Straight out of the oven, straight to you.</span>
       </h1>
     </div>
   );
