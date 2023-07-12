7ca0e5803fa3d9c9328a1504e3ff2f0f6c6ace11
lahirujay99 <lahirujayawardana94@gmail.com>
2023-07-12 08:07:10 +0530

    fixed prettier dependancy issue,text size to 3Xl

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,8 +2,8 @@ import CreateUser from '../features/user/CreateUser'
 
 function Home() {
   return (
-    <div className='my-10 text-center sm:my-16'>
-      <h1 className="mb-8  text-xl font-semibold ">
+    <div className='my-10 text-center sm:my-16 px-4'>
+      <h1 className="mb-8  text-xl font-semibold md:text-3xl">
       {/* <h1 className="text-center text-[100px] font-semibold "> */}
         The best pizza.
         <br />
